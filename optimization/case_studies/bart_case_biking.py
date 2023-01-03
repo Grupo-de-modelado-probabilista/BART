e36a020fc0783bfc7941d6f1c86c84daffe43f16
Franco Joaquín Loyola <fjloyola@users.noreply.github.com>
2023-01-03 07:29:57 -0300

    New directory to test performance optimization of bart (#40)
    * Se agrega el directorio optimization, en el mismo se incluye script para medir performance en tiempo y memoria de bart. Para ello se adaptaron 4 modelos previamente creados en los expermientos:
    - biking;
    - coal;
    - friedman;
    - space_influenza;
    A su vez se genera el .gitignore para evitar que se suban todas las tests debido al peso que implicarian en el repositorio.
    
    * Update optimization/case_studies/bart_case_friedman.py
    
    Se realiza ajuste de Code Review
    
    Co-authored-by: Osvaldo A Martin <aloctavodia@gmail.com>
    
    Co-authored-by: Osvaldo A Martin <aloctavodia@gmail.com>

---
+++ optimization/case_studies/bart_case_biking.py
@@ -0,0 +1,32 @@
+# This script is an adaptation from
+# https://github.com/Grupo-de-modelado-probabilista/BART/blob/master/experiments/Bikes.ipynb
+
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+import pymc as pm
+import pymc_bart as pmb
+
+import helper
+
+args = helper.parse_args()
+
+RANDOM_SEED = 8457
+rng = np.random.RandomState(RANDOM_SEED)
+
+bikes = pd.read_csv(Path("...", "experiments", "bikes.csv"))
+
+X = bikes[["hour", "temperature", "humidity", "windspeed"]]
+Y = bikes["count"]
+
+try:
+    with pm.Model() as model:
+        σ = pm.HalfNormal("σ", Y.std())
+        μ = pmb.BART("μ", X, Y, m=args.trees)
+        y = pm.Normal("y", μ, σ, observed=Y)
+        pm.sample(
+            step=[pmb.PGBART([μ], num_particles=args.particle)], random_seed=RANDOM_SEED
+        )
+except Exception as e:
+    raise RuntimeError("Issue running model") from e
